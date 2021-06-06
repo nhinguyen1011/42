10e3eb58d077f22c96dfb6635aff63447e8e34ec
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2021-06-06 16:53:46 +0700

    Add files via upload

---
+++ Bai42.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Bai42 {
+    public static void main(String[] args){
+        int[] numbers={5,5,5};
+        for (int i=0;i< numbers.lenght;i++){
+            System.out.println(numbers[i]);
+     }
+  }
+}
